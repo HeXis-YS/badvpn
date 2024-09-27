7958e2e661a44c6f76e71fcc15b62601cfb2fb60
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2024-09-27 15:48:25 +0800

    Update lwipopts.h

--- lwip/custom/lwipopts.h
+++ lwip/custom/lwipopts.h
@@ -1,147 +1,360 @@
 /**
- * @file lwipopts.h
- * @author Ambroz Bizjak <ambrop7@gmail.com>
- * 
- * @section LICENSE
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the author nor the
- *    names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * @file
+ *
+ * lwIP Options Configuration
  */
 
-#ifndef LWIP_CUSTOM_LWIPOPTS_H
-#define LWIP_CUSTOM_LWIPOPTS_H
+/*
+ * Copyright (c) 2001-2004 Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ * This file is part of the lwIP TCP/IP stack.
+ *
+ * Author: Adam Dunkels <adam@sics.se>
+ *
+ */
+#ifndef LWIP_LWIPOPTS_H
+#define LWIP_LWIPOPTS_H
 
-// Infrastructure
-// NO_SYS
-#define NO_SYS 1
-// Timers
-#define LWIP_TIMERS 0
-// Heap and memory pools
-#define MEM_LIBC_MALLOC 1
-#define MEMP_MEM_MALLOC 1
-#define MEM_ALIGNMENT 4U
-#define MEMP_OVERFLOW_CHECK 0
-#define MEMP_SANITY_CHECK 0
-// Internal memory pools
-#define MEMP_NUM_PBUF 1024
-#define MEMP_NUM_TCP_PCB 4096
-#define MEMP_NUM_TCP_PCB_LISTEN 16
-#define MEMP_NUM_TCP_SEG 8192
-// Checksum
-#define CHECKSUM_CHECK_IP 0
-#define CHECKSUM_CHECK_TCP 0
-#define CHECKSUM_CHECK_ICMP6 0
-#define LWIP_CHECKSUM_ON_COPY 1
+/*
+   -----------------------------------------------
+   ---------- Platform specific locking ----------
+   -----------------------------------------------
+*/
 
+/**
+ * SYS_LIGHTWEIGHT_PROT==1: if you want inter-task protection for certain
+ * critical regions during buffer allocation, deallocation and memory
+ * allocation and deallocation.
+ */
+#define SYS_LIGHTWEIGHT_PROT            0
 
-// IPv4
-#define LWIP_IPV4 1
-#define IP_FORWARD 0
-#define IP_REASSEMBLY 0
-#define IP_FRAG 0
-// ARP
-#define LWIP_ARP 0
-// ICMP
-#define LWIP_ICMP 0
-// DHCP
-#define LWIP_DHCP 0
-// AUTOIP
-#define LWIP_AUTOIP 0
-// IGMP
-#define LWIP_IGMP 0
+/**
+ * NO_SYS==1: Provides VERY minimal functionality. Otherwise,
+ * use lwIP facilities.
+ */
+#define NO_SYS                          1
 
+/**
+ * LWIP_TIMERS==0: Drop support for sys_timeout and lwip-internal cyclic timers.
+ * (the array of lwip-internal cyclic timers is still provided)
+ * (check NO_SYS_NO_TIMERS for compatibility to old versions)
+ */
+#define LWIP_TIMERS                     0
 
-// IPv6
-#define LWIP_IPV6 1
-#define LWIP_IPV6_AUTOCONFIG 0
-#define LWIP_IPV6_FRAG 0
-#define LWIP_IPV6_REASS 0
-// ICMP6
-#define LWIP_ICMP6 1
-// Multicast listener discovery
-#define LWIP_IPV6_MLD 0
-// DHCPv6
-#define LWIP_IPV6_DHCP6 0
-
-
-// Callback-style APIs
-#define LWIP_CALLBACK_API 1
-// RAW
-#define LWIP_RAW 0
-// DNS
-#define LWIP_DNS 0
-// TCP
-#define LWIP_TCP 1
-#define TCP_MSS 8191
-#define TCP_WND (8 * TCP_MSS)
-#define TCP_SND_BUF (8 * TCP_MSS)
-#define TCP_SND_QUEUELEN ((128 * (TCP_SND_BUF) + (TCP_MSS - 1))/(TCP_MSS))
-// UDP
-#define LWIP_UDP 0
-
-
-// Thread-safe APIs
-// Netconn
-#define LWIP_NETCONN 0
-// Sockets
-#define LWIP_SOCKET 0
-
-
-// PBUF
-#define PBUF_POOL_SIZE 32
-
-
-// NETIF
-#define LWIP_NETIF_API 0
-// Loopback interface
-#define LWIP_HAVE_LOOPIF 0
-#define LWIP_NETIF_LOOPBACK 0
-
-
-// Debugging
-// #define LWIP_DEBUG 1
-// Assertion handling
-// #define LWIP_NOASSERT
-// Performance
-#define LWIP_PERF 0
-// Statistics
-#define LWIP_STATS 0
-// Debug messages
-// #define IP_DEBUG LWIP_DBG_ON
-// #define NETIF_DEBUG LWIP_DBG_ON
-// #define TCP_DEBUG LWIP_DBG_ON
-// #define TCP_INPUT_DEBUG LWIP_DBG_ON
-// #define TCP_OUTPUT_DEBUG LWIP_DBG_ON
-
-
-// Platform specific locking
-#define SYS_LIGHTWEIGHT_PROT 0
-//
+/**
+ * LWIP_DONT_PROVIDE_BYTEORDER_FUNCTIONS: Don't provides byte order
+ * functions.
+ */
+#ifndef LWIP_DONT_PROVIDE_BYTEORDER_FUNCTIONS
 #define LWIP_DONT_PROVIDE_BYTEORDER_FUNCTIONS
-// SNMP
-#define LWIP_SNMP 0
-// PPP
-#define PPP_SUPPORT 0
-//
-#define LWIP_HAVE_SLIPIF 0
-
 #endif
+
+/*
+   ------------------------------------
+   ---------- Memory options ----------
+   ------------------------------------
+*/
+
+/**
+ * MEM_ALIGNMENT: should be set to the alignment of the CPU
+ *    4 byte alignment -> #define MEM_ALIGNMENT 4
+ *    2 byte alignment -> #define MEM_ALIGNMENT 2
+ */
+#define MEM_ALIGNMENT                   4U
+
+/**
+ * MEM_LIBC_MALLOC==1: Use malloc/free/realloc provided by your C-library
+ * instead of the lwip internal allocator. Can save code size if you
+ * already use it.
+ */
+#define MEM_LIBC_MALLOC                 1
+
+/*
+   ------------------------------------------------
+   ---------- Internal Memory Pool Sizes ----------
+   ------------------------------------------------
+*/
+/**
+ * MEMP_NUM_PBUF: the number of memp struct pbufs (used for PBUF_ROM and PBUF_REF).
+ * If the application sends a lot of data out of ROM (or other static memory),
+ * this should be set high.
+ */
+#define MEMP_NUM_PBUF                   8192
+
+/**
+ * MEMP_NUM_TCP_PCB: the number of simulatenously active TCP connections.
+ * (requires the LWIP_TCP option)
+ */
+#define MEMP_NUM_TCP_PCB                4096
+
+/**
+ * MEMP_NUM_TCP_PCB_LISTEN: the number of listening TCP connections.
+ * (requires the LWIP_TCP option)
+ */
+#define MEMP_NUM_TCP_PCB_LISTEN         16
+
+/**
+ * MEMP_NUM_TCP_SEG: the number of simultaneously queued TCP segments.
+ * (requires the LWIP_TCP option)
+ */
+#define MEMP_NUM_TCP_SEG                8192
+
+/**
+ * MEMP_NUM_REASSDATA: the number of simultaneously IP packets queued for
+ * reassembly (whole packets, not fragments!)
+ */
+#define MEMP_NUM_REASSDATA              1
+
+/**
+ * PBUF_POOL_SIZE: the number of buffers in the pbuf pool.
+ */
+#define PBUF_POOL_SIZE                  32
+
+/*
+   ---------------------------------
+   ---------- ARP options ----------
+   ---------------------------------
+*/
+/**
+ * LWIP_ARP==1: Enable ARP functionality.
+ */
+#define LWIP_ARP                        0
+
+/*
+   --------------------------------
+   ---------- IP options ----------
+   --------------------------------
+*/
+
+/**
+ * IP_REASSEMBLY==1: Reassemble incoming fragmented IP packets. Note that
+ * this option does not affect outgoing packet sizes, which can be controlled
+ * via IP_FRAG.
+ */
+#define IP_REASSEMBLY                   1
+
+/**
+ * IP_FRAG==1: Fragment outgoing IP packets if their size exceeds MTU. Note
+ * that this option does not affect incoming packet sizes, which can be
+ * controlled via IP_REASSEMBLY.
+ */
+#define IP_FRAG                         0
+
+/**
+ * IP_REASS_MAXAGE: Maximum time (in multiples of IP_TMR_INTERVAL - so seconds, normally)
+ * a fragmented IP packet waits for all fragments to arrive. If not all fragments arrived
+ * in this time, the whole packet is discarded.
+ */
+#define IP_REASS_MAXAGE                 3
+
+/**
+ * IP_REASS_MAX_PBUFS: Total maximum amount of pbufs waiting to be reassembled.
+ * Since the received pbufs are enqueued, be sure to configure
+ * PBUF_POOL_SIZE > IP_REASS_MAX_PBUFS so that the stack is still able to receive
+ * packets even if the maximum amount of fragments is enqueued for reassembly!
+ */
+#define IP_REASS_MAX_PBUFS              4
+
+/*
+   ---------------------------------------
+   ---------- IPv6 options ---------------
+   ---------------------------------------
+*/
+/**
+ * LWIP_IPV6==1: Enable IPv6 module.
+ */
+#define LWIP_IPV6                       1
+
+/** IP6_FRAG_COPYHEADER==1: for platforms where sizeof(void*) > 4, "struct
+ * ip6_reass_helper" is too large to be stored in the IPv6 fragment header, and
+ * will bleed into the header before it, which may be the IPv6 header or an
+ * extension header. This means that for each first fragment packet, we need to
+ * 1) make a copy of some IPv6 header fields (src+dest) that we need later on,
+ * just in case we do overwrite part of the IPv6 header, and 2) make a copy of
+ * the header data that we overwrote, so that we can restore it before either
+ * completing reassembly or sending an ICMPv6 reply. The last part is true even
+ * if this setting is disabled, but if it is enabled, we need to save a bit
+ * more data (up to the size of a pointer) because we overwrite more. */
+#ifdef __LP64__
+#define IPV6_FRAG_COPYHEADER            1
+#else
+#define IPV6_FRAG_COPYHEADER            0
+#endif
+
+/**
+ * LWIP_IPV6_FRAG==1: Fragment outgoing IPv6 packets that are too big.
+ */
+#define LWIP_IPV6_FRAG                  0
+
+/**
+ * LWIP_IPV6_MLD==1: Enable multicast listener discovery protocol.
+ */
+#define LWIP_IPV6_MLD                   0
+
+/*
+   ----------------------------------
+   ---------- ICMP options ----------
+   ----------------------------------
+*/
+/**
+ * LWIP_ICMP==1: Enable ICMP module inside the IP stack.
+ * Be careful, disable that make your product non-compliant to RFC1122
+ */
+#define LWIP_ICMP                       0
+
+/*
+   ---------------------------------
+   ---------- UDP options ----------
+   ---------------------------------
+*/
+/**
+ * LWIP_UDP==1: Turn on UDP.
+ */
+#define LWIP_UDP                        0
+
+/*
+   ---------------------------------
+   ---------- TCP options ----------
+   ---------------------------------
+*/
+/**
+ * LWIP_TCP==1: Turn on TCP.
+ */
+#define LWIP_TCP                        1
+
+/**
+ * TCP_MSS: TCP Maximum segment size.
+ * For the receive side, this MSS is advertised to the remote side
+ * when opening a connection. For the transmit size, this MSS sets
+ * an upper limit on the MSS advertised by the remote host.
+ */
+#define TCP_MSS                         8191
+
+/**
+ * TCP_WND: The size of a TCP window.  This must be at least
+ * (2 * TCP_MSS) for things to work well.
+ * ATTENTION: when using TCP_RCV_SCALE, TCP_WND is the total size
+ * with scaling applied. Maximum window value in the TCP header
+ * will be TCP_WND >> TCP_RCV_SCALE
+ */
+#define TCP_WND                         (8 * TCP_MSS)
+
+/**
+ * TCP_SND_BUF: TCP sender buffer space (bytes).
+ * To achieve good performance, this should be at least 2 * TCP_MSS.
+ */
+#define TCP_SND_BUF                     (8 * TCP_MSS)
+
+/**
+ * TCP_SND_QUEUELEN: TCP sender buffer space (pbufs). This must be at least
+ * as much as (2 * TCP_SND_BUF/TCP_MSS) for things to work.
+ */
+#define TCP_SND_QUEUELEN                ((128 * (TCP_SND_BUF) + (TCP_MSS - 1)) / (TCP_MSS))
+
+/*
+   ----------------------------------
+   ---------- Pbuf options ----------
+   ----------------------------------
+*/
+/**
+ * PBUF_LINK_HLEN: the number of bytes that should be allocated for a
+ * link level header. The default is 14, the standard value for
+ * Ethernet.
+ */
+#define PBUF_LINK_HLEN                  16
+
+/**
+ * PBUF_POOL_BUFSIZE: the size of each pbuf in the pbuf pool. The default is
+ * designed to accommodate single full size TCP frame in one pbuf, including
+ * TCP_MSS, IP header, and link header.
+*
+ */
+#define PBUF_POOL_BUFSIZE               LWIP_MEM_ALIGN_SIZE(TCP_MSS + 40 + PBUF_LINK_HLEN)
+
+/*
+   ----------------------------------------------
+   ---------- Sequential layer options ----------
+   ----------------------------------------------
+*/
+
+/**
+ * LWIP_NETCONN==1: Enable Netconn API (require to use api_lib.c)
+ */
+#define LWIP_NETCONN                    0
+
+/*
+   ------------------------------------
+   ---------- Socket options ----------
+   ------------------------------------
+*/
+/**
+ * LWIP_SOCKET==1: Enable Socket API (require to use sockets.c)
+ */
+#define LWIP_SOCKET                     0
+
+/**
+ * SO_REUSE==1: Enable SO_REUSEADDR
+ */
+#define SO_REUSE                        1
+
+/*
+   ----------------------------------------
+   ---------- Statistics options ----------
+   ----------------------------------------
+*/
+/**
+ * LWIP_STATS==1: Enable statistics collection in lwip_stats.
+ */
+#define LWIP_STATS                      0
+
+/*
+   --------------------------------------
+   ---------- Checksum options ----------
+   --------------------------------------
+*/
+
+/**
+ * CHECKSUM_CHECK_IP==1: Check checksums in software for incoming IP packets.
+ */
+#define CHECKSUM_CHECK_IP               0
+
+/**
+ * CHECKSUM_CHECK_TCP==1: Check checksums in software for incoming TCP packets.
+ */
+#define CHECKSUM_CHECK_TCP              0
+
+/**
+ * CHECKSUM_CHECK_ICMP6==1: Check checksums in software for incoming ICMPv6 packets
+ */
+#define CHECKSUM_CHECK_ICMP6            0
+
+/**
+ * LWIP_CHECKSUM_ON_COPY==1: Calculate checksum when copying data from
+ * application buffers to pbufs.
+ */
+#define LWIP_CHECKSUM_ON_COPY           1
+
+#endif /* LWIP_LWIPOPTS_H */
--- tun2socks-android-cortex-a55.mk
+++ tun2socks-android-cortex-a55.mk
@@ -8,10 +8,10 @@ ROOT_PATH := $(LOCAL_PATH)
 include $(CLEAR_VARS)
 
 LOCAL_LDFLAGS := -Wno-unused-command-line-argument
-LOCAL_LDFLAGS += -Ofast -mcpu=cortex-a55+crypto+ssbs -mtune=cortex-a55 -flto=full -fno-common -fno-plt -fno-semantic-interposition -fcf-protection=none
+LOCAL_LDFLAGS += -fPIE -Ofast -mcpu=cortex-a55+crypto+ssbs -mtune=cortex-a55 -flto=full -fno-common -fno-plt -fno-semantic-interposition -fcf-protection=none
 LOCAL_LDFLAGS += -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-ast-use-context -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-2nd-level-tiling -mllvm -polly-pattern-matching-based-opts -mllvm -polly-position=before-vectorizer -mllvm -polly-invariant-load-hoisting
 LOCAL_LDFLAGS += -ftls-model=initial-exec -fno-builtin-malloc
-LOCAL_LDFLAGS += -fuse-ld=lld -s -Wl,-O3,--as-needed,--gc-sections,--icf=all,-z,lazy,-z,norelro,-sort-common
+LOCAL_LDFLAGS += -fuse-ld=lld -s -Wl,-O3,--as-needed,--icf=all,-z,lazy,-z,norelro,-sort-common
 # LOCAL_LDFLAGS += -static -Wl,-Bdynamic,-llog,-Bstatic,-ldl
 LOCAL_LDFLAGS += -llog -ldl
 LOCAL_LDFLAGS += -Wl,--gc-sections
